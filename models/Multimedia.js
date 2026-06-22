514cca1dd6d2ce3c523873da0646fff4d77f0298
Alexis Solis <solisalexis7825@gmail.com>
2026-06-21 18:54:14 -0600

    docs: asegurar esquema de Mongoose para Multimedia

--- models/Multimedia.js
+++ models/Multimedia.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const ElementoMultimediaSchema = new mongoose.Schema({
     titulo: { type: String, required: true },
     descripcion: String,
-    imagenUrl: { type: String, required: true }, // Guarda la ruta pública/local [cite: 134]
-    audioUrl: { type: String, required: true },  // Guarda la ruta pública/local [cite: 134]
-    tags: [String],                             // Reto de optimización NoSQL [cite: 154]
+    imagenUrl: { type: String, required: true }, // Guarda la ruta pública/local
+    audioUrl: { type: String, required: true },  // Guarda la ruta pública/local
+    tags: [String],                             // Reto de optimización NoSQL
     fechaCreacion: { type: Date, default: Date.now }
 });
 
